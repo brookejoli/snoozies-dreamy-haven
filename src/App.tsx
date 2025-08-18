2db76031da94b532942c6737ff1f279e0ddf8262
brookejoli <brookejoli@gmail.com>
2025-08-18 16:01:18 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,5 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
-import Home from "./pages/Home";
-import Stories from "./pages/Stories";
-import StorySuggestions from "./pages/StorySuggestions";
-
-
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <div className="min-h-screen bg-background">
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/stories" element={<Stories />} />
-            <Route path="/story-suggestions" element={<StorySuggestions />} />
-            
-            
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/stories" element={<Stories />} />
+  <Route path="/stories/:slug" element={<StoryDetail />} />
+</Routes>
